496a6a6092504e648c278a3569b64ca1adadc390
Younn <younnes.dev@gmail.com>
2024-01-14 12:31:57 +0100

    Date : 14/01/2024 Heure : 12:30 Simple application qui va afiicher Hello World en MessageBox sur Windows en C#.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Hello_World
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            MessageBox.Show(@"Hello World !", @"by amn...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
